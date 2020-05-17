9cc1f5249b4f7cb2f9350f084f53cf3f55bc836b
xueqing618 <3419590244@qq.com>
2020-05-17 22:14:11 +0800

    '添加'

--- src/pages/home/three/index.jsx
+++ src/pages/home/three/index.jsx
@@ -1,10 +1,68 @@
 import React, { Component } from 'react'
+import { Form, Input, Button } from 'antd';
+import {post} from '../../../untils/request'
+import api from '@/services/api'
 
-export default class index extends Component {
+export default @Form.create({
+  // 表单回填使用
+  mapPropsToFields () {
+    return {
+      
+    }
+  },
+})
+
+class index extends Component {
+  handleReset = () => {
+    this.props.form.resetFields();
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(api.add)
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        post(api.add, values).then(res => {
+          if (res.status == 200) {
+            alert(res.info)
+            this.props.history.push('/home/two')
+          }
+        })
+      }
+    });
+  };
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div>
-        
+      <div className="Add">
+        <Form onSubmit={this.handleSubmit} >
+          <Form.Item label="Name" >
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: '请输入用户名' }],
+            })(
+              <Input placeholder="Name" />
+            )}
+          </Form.Item>
+          <Form.Item label="Age">
+          {getFieldDecorator('age', {
+              rules: [{ required: true, message: '请输入年龄' }],
+            })(
+              <Input placeholder="Age" />
+            )}
+          </Form.Item>
+          <Form.Item label="Msg">
+            {getFieldDecorator('msg', {
+              rules: [{ required: true, message: '请输入价格' }],
+            })(
+              <Input placeholder="Msg" />
+            )}
+          </Form.Item>
+          <Form.Item >
+            <Button type="primary" htmlType="submit" >Submit</Button>
+            <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
+              Clear
+            </Button>          
+          </Form.Item>
+        </Form>
       </div>
     )
   }
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 export default{
   list:'https://blogs.zdldove.top/Home/Apis/listWithPage',
-  serach:"https://api.baxiaobu.com/index.php/home/v5/findUser"
+  serach:"https://api.baxiaobu.com/index.php/home/v5/findUser",
+  add:"https://api.baxiaobu.com/index.php/home/v5/add"
 }
